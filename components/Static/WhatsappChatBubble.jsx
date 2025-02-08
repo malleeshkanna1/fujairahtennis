213538238c49097b563bedbb9330d9e768a013c6
Malleeshkanna M <malleeshkanna1@gmail.com>
2025-02-08 07:40:16 +0530

    whatsapp api update

--- components/Static/WhatsappChatBubble.jsx
+++ components/Static/WhatsappChatBubble.jsx
@@ -4,7 +4,7 @@ import WhatsappChatBubbleStyle from './Whatsapppop.module.css';
 const WhatsappChatBubble = () => {
     return (
       <a
-        href="https://web.whatsapp.com/send?phone=97192244880&text="
+        href="https://api.whatsapp.com/send?phone=919442668758&text="
         target="_blank"
         rel="noopener noreferrer"
         className={WhatsappChatBubbleStyle["whatsapp-chat-bubble"]}
